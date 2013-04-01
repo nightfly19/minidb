49ab01839e6ba5f88af6b778be76825e7ea8689f
Sage <sage@sagenite.net>
2013-04-01 16:31:58 -0700

    Rewrote queryCompiler to use multimethods (untested)

--- query_compiler.js
+++ query_compiler.js
@@ -1,52 +1,66 @@
-var compile;
+var multimethod = require('multimethod');
 
-exports.compile = compile = function(query){
-    var queryc = query.slice(0);
-    var first = queryc[0];
-    queryc.shift();
-    switch (first){
-    case 'and':
-        if(queryc.length > 1){
-            var temp_fn_a = compile(queryc[0]);
-            queryc[0] = 'and';
-            var temp_fn_b = compile(queryc);
+var compiler, compile;
+
+var isForm = function(form){Array.isArray(form)};
+
+var formExpand = function(form, callback){
+    var car = query.slice(0);
+    var cdr = queryc[0];
+    cdr.shift();
+    return callback(car, cdr);
+};
+
+exports.compile = compile = multimethod().dispatch(isForm)
+    .when(true,function(form){
+        return formExpand(form,function(symbol, args){return compiler(symbol, args);});
+    })
+    .when(false,function(form){
+        return function(){ return form;}
+    });
+
+compiler = multimethod().dispatch(function(symbol, args){return symbol;})
+    .when('and', function(symbol, args){
+        if(args.length > 1){
+            var temp_fn_a = compile(args[0]);
+            args[0] = 'and';
+            var temp_fn_b = compile(args);
             return function(resource, certname){
-                return temp_fn_a(resource, certname) && temp_fn_b(resource, certname);};
+                return temp_fn_a(resource, certname) && temp_fn_b(resource, certname);
+            };
         }
         else{
-            return compile(queryc[0]);
+            return compile(args[0]);
         }
-        break;
-    case '=':
-        var field = queryc[0];
-        var value = queryc[1];
-        switch (field){
-        case 'certname':
-            return function(resource, certname){return value == certname;};
-            break;
-        case 'tag':
-            return function(resource, certname){
-                return (resource.tags.indexOf(value) != -1) ? true : false;};
-            break;
-        default:
-            if (Array.isArray(field)){
-                return function(resource, certname){return resource[field[1]] == value;};
-            }
-            else{
-                return function(resource, certname){return resource[field] == value;};
-            }
-            break;
-        }
-        break;
-    case 'not':
-        var temp_fn = compile(queryc[0]);
+    })
+    .when('not', function(symbol, args){
+        var temp_fn = compile(args[0]);
         return function(resource, certname){
-            return !temp_fn(resource, certname);}
-        break;
-    default:
-        console.log("Fail");
-        console.log(first);
-        console.log(query);
-        return queryc;
-    }
-};
+            return !temp_fn(resource, certname);
+        }
+    })
+    .when('=', (function(){
+        var eqlDispatch = multimethod().dispatch(function(field, value){return value;})
+            .when('certname', function(field, value){
+                return function(resource, certname){return value == certname;};
+            })
+            .when('tag', function(field, value){
+                return function(resource, certname){
+                    return (resource.tags.indexOf(value) != -1) ? true : false;
+                };
+            })
+            .default( function(field, value){
+                if (Array.isArray(field)){
+                    return function(resource, certname){return resource[field[1]] == value;};
+                }
+                else{
+                    return function(resource, certname){return resource[field] == value;};
+                }
+            });
+
+        return function(symbol, args){
+            var field = args[0];
+            var value = args[1];
+            return eqlDispatch(field, value);
+        };
+    })());
